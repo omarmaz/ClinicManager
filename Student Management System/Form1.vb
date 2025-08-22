0f775c4cfc4bceb2ddb5fb12a95b094fdfb3886a
user <user@DESKTOP-AJ70G6N>
2025-08-22 18:47:12 +0300

    Add project files.

---
+++ Student Management System/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim conn As New MySqlConnection("server=localhost;userid=root;password=omarmazen2004;database=studentsdb")
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            conn.Open()
+            MessageBox.Show("Connection Successful!")
+            conn.Close()
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        End Try
+    End Sub
+End Class
+
+
+
